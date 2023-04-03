6f0abd0d82fc6883b2c62aaf324d36d63b9f6b58
Willian Del Freu <williankeyex@gmail.com>
2023-04-02 21:38:07 -0300

    Integration with Stripe - Format Price

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from './styles/global'
+
+import logoImg from '../assets/logo.svg';
+import Image from 'next/image';
+import { Container, Header } from './styles/pages/app';
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
