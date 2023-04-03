7c8068cb5b7b246489ec43e0c54f093ce1ff8d51
Willian Del Freu <williankeyex@gmail.com>
2023-04-03 19:49:34 -0300

    Product & Checkout - Navigation to Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { HomeContainer, Product } from "./styles/pages/home";
 import { useKeenSlider } from 'keen-slider/react';
 import { stripe } from "../lib/stripe";
@@ -30,16 +31,17 @@ const [sliderRef] = useKeenSlider({
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-                
-                <Product key={product.id} className="keen-slider__slide">
+          <Link href={`/product/${product.id}`} passHref legacyBehavior key={product.id}>     
+            <Product key={product.id} className="keen-slider__slide">
                   
-                  <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-                  <footer>
-                    <strong>{product.name}</strong>
-                    <span>{product.price}</span>
-                  </footer>
-                </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link> 
         )
       })}
     </HomeContainer>
