478a439cc877e57b326adcf0f29e9ae5471ff163
Willian Del Freu <williankeyex@gmail.com>
2023-04-03 20:10:30 -0300

    Product & Checkout - Structure page of product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,20 @@
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../styles/pages/product"
 
 export default function Product() {
     const { query } = useRouter()
     return (
-        <h1>Product</h1>
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, quisquam. Numquam aspernatur rem eius hic beatae? Maiores sapiente numquam esse dolores qui laboriosam. Adipisci, sit. Eum veniam tempora omnis itaque?</p>
+                <button>Comprar Agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
